fa5d3162a213b9967b29f46b8380761db70042d6
lshi82631 <lshi8263@inst.hcpss.org>
2025-11-21 11:43:35 -0500

    heloo

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+let expressions = [''];
+const priority = ['*','/','+','-'];
+
 function type (input) {
     let inputBox = document.getElementById("inputBox");
-    inputBox.innerHTML+=input;
+    inputBox.innerHTML+=input + " ";
+    if (priority.includes(input)){
+        expressions[count] += input;
+        count++;
+    }
 }
 function solve(){
-    let inputBox = document.getElementById("inputBox");
-    let list = inputBox.innerText;
+    for(let i = 0; i < list.length; i++) {
+        for(let j = 0; j < expressions.length;j++) {
+            if()expressions[j] += list.charAt(i);
+        }
+    }
 }
